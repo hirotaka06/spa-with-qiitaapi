55cde9e6bae7e64c04f19813276ca683ea1504d1
hirotaka06 <h.kojima572@gmail.com>
2025-02-21 14:37:52 +0900

    Add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Hello World</h1>
+      <Button className="bg-blue-500 text-white">Click me</Button>
     </div>
   );
 }
